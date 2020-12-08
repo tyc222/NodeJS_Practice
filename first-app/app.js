ca6fc778abc8d0adb3fb806e1ed72c4c46ecfabc
tc <timothy.c@amaryllo.eu>
2020-12-08 11:54:09 +0800

    setup controller for MVC

--- first-app/app.js
+++ first-app/app.js
@@ -8,17 +8,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const errorController = require('./controllers/error')
+
+const adminDataRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminDataRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
