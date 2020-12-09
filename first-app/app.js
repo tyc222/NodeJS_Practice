7e2232424f97c3b9800269a7db49ad5f7027253a
tc <timothy.c@amaryllo.eu>
2020-12-09 16:18:30 +0800

    refactor to MVC

--- first-app/app.js
+++ first-app/app.js
@@ -3,20 +3,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const errorController = require('./controllers/error')
-
-const adminDataRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminDataRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
